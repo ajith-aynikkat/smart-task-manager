8311fabbbc13dcc0551f1548fac4fc422bea7978
ajith <ajith.aynikkat@gmail.com>
2025-12-13 23:33:14 +0530

    error commit

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,27 +1,46 @@
-const API="http://localhost:5000/tasks";
-const t=localStorage.getItem("token");
+const API = "/api/tasks";
+const t = localStorage.getItem("token");
 
 function add(){
- fetch(API,{method:"POST",
- headers:{"Content-Type":"application/json","Authorization":"Bearer "+t},
- body:JSON.stringify({title:title.value,due_date:due.value,priority:priority.value})
+ fetch(API,{
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json",
+    "Authorization":"Bearer "+t
+  },
+  body:JSON.stringify({
+    title:title.value,
+    due_date:due.value,
+    priority:priority.value
+  })
  }).then(load);
 }
 
 function load(){
- fetch(API,{headers:{Authorization:"Bearer "+t}})
- .then(r=>r.json()).then(d=>{
+ fetch(API,{
+  headers:{
+    "Authorization":"Bearer "+t
+  }
+ })
+ .then(r=>r.json())
+ .then(d=>{
   list.innerHTML="";
   d.forEach(x=>{
-   list.innerHTML+=`<li>${x.title} ${x.status}
-   <button onclick="done(${x.id})">✔</button></li>`;
-  })
- })
+   list.innerHTML+=`
+     <li>${x.title} ${x.status}
+       <button onclick="done(${x.id})">✔</button>
+     </li>`;
+  });
+ });
 }
 
 function done(id){
- fetch(`${API}/${id}/complete`,{method:"PUT",headers:{Authorization:"Bearer "+t}})
- .then(load)
+ fetch(`/api/tasks/${id}/complete`,{
+  method:"PUT",
+  headers:{
+    "Authorization":"Bearer "+t
+  }
+ }).then(load);
 }
 
 load();
